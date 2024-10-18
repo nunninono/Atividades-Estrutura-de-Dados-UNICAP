51c2a592918a9048d35e5f62418cf035fde5cfb6
Nunno Wakiyama Diniz Carvalho <142551010+nunninono@users.noreply.github.com>
2024-10-18 09:51:33 -0300

    Update Q05 outra aplicação.c

--- Lista 9/Q05 outra aplicação.c
+++ Lista 9/Q05 outra aplicação.c
@@ -3,10 +3,34 @@
 #include <string.h>
 
 typedef struct tarefa {
-  char descricao [100];
-  int prioridade;
+    char descricao [100];
+    int prioridade;
 } Tarefa;
 
 typedef struct ldeNode {
-  
+    struct ldeNode* ant;
+    Tarefa* info;
+    struct ldeNode* prox;
+} LDENode;
+
+typedef struct lde {
+    LDENode* inicio;
+    LDENode* fim;
+    int qtd;
+} LDE;
+
+void inicializar (LDE* lista) {
+    lista->inicio = NULL;
+    lista->fim = NULL;
+    lista->qtd = 0;
 }
+
+int isEmpty (LDE lista) {
+    if (lista->inicio == NULL && lista->fim == NULL) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
