33ad8a01abb6c6773f5c3a0b9005290be92bee05
Nunno Wakiyama Diniz Carvalho <142551010+nunninono@users.noreply.github.com>
2024-10-19 23:16:12 -0300

    Update Q01.c

--- Lista 10/Q01.c
+++ Lista 10/Q01.c
@@ -8,4 +8,26 @@ typedef struct info {
 typedef struct ldeNode {
     struct ldeNode* ant;
     Info* info;
+    struct ldeNode* prox;
 } LDENode;
+
+typedef struct lde {
+    ListNode* inicio;
+    ListNode* fim;
+    int qtd;
+} LDE;
+
+void inicializar (LDE* lista) {
+    lista->inicio = NULL;
+    lista->fim = NULL;
+    lista->qtd = 0;
+}
+
+int isEmpty (LDE lista) {
+    if (lista.inicio == NULL && lista.fim == NULL) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
