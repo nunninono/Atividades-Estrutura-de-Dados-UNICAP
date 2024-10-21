e7ba4f7bdf429e185e4c81c569a57c946242adf2
Nunno Wakiyama Diniz Carvalho <142551010+nunninono@users.noreply.github.com>
2024-10-20 23:43:13 -0300

    Update Q01.c

--- Lista 10/Q01.c
+++ Lista 10/Q01.c
@@ -1,3 +1,5 @@
+// FALTA A MAIN
+
 #include <stdio.h>
 #include <stlib.h>
 
@@ -38,16 +40,69 @@ Info* criarInfo (int valor) {
     return novoInfo;
 }
 
-LDENode* criarNo () {
+LDENode* criarNo (Info* novoInfo) {
     novoNo = (LDENode*) malloc (sizeof(LDENode*));
     novoNo->ant = NULL;
     novoNo->prox = NULL;
-    novoNo->info = 
+    novoNo->info = noveInfo;
+    return novoNo;
 }
 
-void inserirFinal (LDE* lista) {
-    
+void inserirFinal (LDE* lista, int valor) {
+    Info* novoInfo = criarInfo(valor);
+    LDENode* novoNo = criarNo(novoInfo);
     if (isEmpty(*lista) == 1) {
-        
+        lista->inicio = novoNo;
+        lista->fim = novoNo;
+    }
+    else {
+        novoNo->ant = lista->fim;
+        lista->fim->prox = novoNo;
+        lista->fim = novoNo;
+    }
+    lista->qtd++;
+    printf("Valor foi inserido na lista!\n");
+}
+
+void remover (LDE* lista, int valor) {
+    if (isEmpty(*lista) == 1) {
+        printf("A lista esta vazia!\n");
+        return;
+    }
+    for (ListNode* aux = lista->inicio; aux != NULL; aux = aux->prox) {
+        int count_achados = 0;
+        int count_nao_achados = 0;
+        if (aux->info->valor == valor) {
+            if (aux == lista->inicio) {
+                lista->inicio = lista->inicio->prox;
+                free(lista->inicio->ant);
+                lista->inicio->ant = NULL;
+                lista->qtd--;
+                count++;
+            }
+            else if (aux == lista->fim) {
+                lista->fim = lista->inicio->ant;
+                free(lista->fim->prox);
+                lista->fim->prox = NULL;
+                lista->qtd--;
+                count++;
+            }
+            else {
+                aux->ant->prox = aux->prox;
+                aux->prox->ant = aux->ant;
+                free(aux);
+                lista->qtd--;
+                count++;
+            }
+        }
+        else {
+            count_nao_achados++;
+        }
+    }
+    if (count_nao_achados == lista->qtd) {
+        printf("Nao foi encontrado nenhum %d na lista!\n", valor);
+    }
+    else {
+        printf("Um total de %d valores foram removidos!!\n", count);
     }
 }
