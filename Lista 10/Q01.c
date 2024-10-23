e61b7e1b247bdfdb74ccb845862f4f0fd1962226
Nunno Wakiyama Diniz Carvalho <142551010+nunninono@users.noreply.github.com>
2024-10-23 19:33:30 -0300

    Update Q01.c

--- Lista 10/Q01.c
+++ Lista 10/Q01.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct lseNode {
+    int info;
+    struct lseNode* prox;
+} LSENode;
+
+typedef struct lse {
+    LSENode* inicio;
+    LSENode* fim;
+    int qtd;
+} LSE;
+
+void isEmpty (LSE lista) {
+    if (lista.inicio == NULL && lista.fim == NULL) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
