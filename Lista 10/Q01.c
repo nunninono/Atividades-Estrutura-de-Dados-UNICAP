650e9c34b848e2f15a83e7f41efa9e8e0f861f79
Nunno Wakiyama Diniz Carvalho <142551010+nunninono@users.noreply.github.com>
2024-10-24 01:31:08 -0300

    Update Q01.c

--- Lista 10/Q01.c
+++ Lista 10/Q01.c
@@ -1,22 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct lseNode {
+typedef struct lsecNode {
     int info;
-    struct lseNode* prox;
-} LSENode;
+    struct lsecNode* prox;
+} LSECNode;
 
-typedef struct lse {
-    LSENode* inicio;
-    LSENode* fim;
+typedef struct lsec {
+    LSECNode* inicio;
+    LSECNode* fim;
     int qtd;
-} LSE;
+} LSEC;
 
-void isEmpty (LSE lista) {
+void inicializar (LSEC* lista) {
+    lista->fim = NULL;
+    lista->inicio = NULL;
+    lista->qtd = 0;
+}
+
+int isEmpty (LSEC lista) {
     if (lista.inicio == NULL && lista.fim == NULL) {
         return 1;
     }
     else {
         return 0;
     }
+}
+
+LSECNode* criarNo (int valor) {
+    LSECNode* novoNo = (LSECNode*) malloc (sizeof(LSECNode));
+    novoNo->prox = NULL;
+    novoNo->info = valor;
+    return novoNo;
+}
+
+int buscaSimples (LSEC lista, int valor) {
+    if (isEmpty(lista) == 1) {
+        return 0;
+    }
+    else {
+        LSECNode* aux = lista.inicio;
+        do {
+            if (aux->info = valor) {
+                return 1;
+            }
+            aux = aux->prox;
+        } while (aux != lista.fim);
+        return 0;
+    }
+}
+
+void inserirInicio (LSEC* lista, int valor) {
+    LSECNode* novoNo = criarNo(valor);
+    if (isEmpty(*lista) == 1) {
+        lista->fim = novoNo;
+        lista->inicio = novoNo;
+        lista->fim->prox = lista->inicio; 
+    }
+    else {
+        if (buscaSimples(*lista, valor) != 1) {
+            novoNo->prox = lista->inicio;
+            lista->inicio = novoNo;
+            lista->fim->prox = lista->inicio;
+        }
+        else {
+            printf("Valor ja presente na lista!!\n");
+            free(novoNo);
+            return;
+        }
+    }
+    lista->qtd++;
+    printf("Valor inserido na lista!!\n");
+}
+
+void inserirFinal (LSEC* lista, int valor) {
+    LSECNode* novoNo = criarNo(valor);
+    if (isEmpty(*lista) == 1) {
+        lista->fim = novoNo;
+        lista->inicio = novoNo;
+        lista->fim->prox = lista->inicio;
+    }
+    else {
+        if (buscaSimples(*lista, valor) != 1) {
+            lista->fim->prox = novoNo;
+            lista->fim = novoNo;
+            lista->fim->prox = lista->inicio;
+        }
+        else {
+            printf("Valor ja presente na lista!!\n");
+            free(novoNo);
+            return;
+        }
+    }
+    lista->qtd++;
+    printf("Valor inserido na lista!!\n");
 }
