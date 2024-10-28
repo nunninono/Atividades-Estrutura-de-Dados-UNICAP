2b6e299143e2d8b5a7b506771dbec4a66592d415
Nunno Wakiyama Diniz Carvalho <142551010+nunninono@users.noreply.github.com>
2024-10-28 19:40:14 -0300

    Update Q01.c

--- Lista 11/Q01.c
+++ Lista 11/Q01.c
@@ -2,13 +2,34 @@
 #include <stdlib.h>
 
 typedef struct dados {
-
+    char nome[100];
+    float media_final;
+    int quantidade_faltas;
 } Dados;
 
 typedef struct ldeNode {
-
-} ldeNode;
+    struct ldeNode* ant;
+    Dados* info;
+    struct ldeNode* prox;
+} LDENode;
 
 typedef struct lde {
-
+    LDENode* inicio;
+    LDENode* fim;
+    int qtd;
 } LDE;
+
+void inicialize (LDE* lista) {
+    lista->inicio = NULL;
+    lista->fim = NULL;
+    lista->qtd = 0;
+}
+
+int isEmpty(LDE* lista) {
+    if (lista->fim == NULL && lista->inicio == NULL) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
