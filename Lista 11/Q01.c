f89de6861845f8802fbe240795d53886546898dc
Nunno Wakiyama Diniz Carvalho <142551010+nunninono@users.noreply.github.com>
2024-10-30 23:59:22 -0300

    Update Q01.c

--- Lista 11/Q01.c
+++ Lista 11/Q01.c
@@ -103,3 +103,89 @@ void cadastrar (LDEC* lista, char* nome, float media_final, int quantidade_falta
         printf("Cadastro realizado!!\n");
     }
 }
+
+void listar (LDEC lista) {
+    if (isEmpty(lista) == 1) {
+        printf("A lista esta vazia!!\n");
+        return;
+    }
+    LDECNode* aux = lista.inicio;
+    int count = 0;
+    printf("Alunos cadastrados:\n\n");
+    do {
+        printf("%d - Nome: %s\n", count, aux->info->nome);
+        printf("%d - Media final: %f\n", count, aux->info->media_final);
+        printf("%d - Quantidade de faltas: %d\n\n", count, aux->info->quantidade_faltas);
+        aux = aux->prox;
+    } while (aux != lista.inicio);
+}
+
+LDECNode* consultar (LDEC lista, char* nome) {
+    if (isEmpty(lista) == 1) {
+        return NULL;
+    }
+    else {
+        LDECNode* aux = lista.inicio;
+        do {
+            if (strcmp(aux->info->nome, nome) == 0) {
+                return aux;
+            }
+            aux = aux->prox;
+        } while (aux != lista.inicio);
+    }
+    return NULL;
+}
+
+void alterarMediaFinal (LDEC* lista, char* nome) {
+    if (isEmpty(*lista) == 1) {
+        printf("Lista vazia!!\n");
+        return;
+    }
+    else {
+        LDECNode* aux = consultar(*lista, nome);
+        if (aux != NULL) {
+            float novaMedia;
+            printf("Insira a nova media final de %s: ", nome);
+            scanf("%f", &novaMedia);
+            printf("\n\n");
+            aux->info->media_final = novaMedia;
+            return;
+        }
+        else {
+            printf("Nao foi possivel realizar a mudanca, nome nao encontrado!\n");
+        }
+    }
+}
+
+void alterarQuantidadeDeFaltas (LDEC* lista, char* nome) {
+    if (isEmpty(*lista) == 1) {
+        printf("Lista vazia!!\n");
+        return;
+    }
+    else {
+        LDECNode* aux = consultar(*lista, nome);
+        if (aux != NULL) {
+            int mudanca;
+            char escolha;
+            printf("Voce quer adicionar(a) ou remover faltas(r) r ou a : ");
+            scanf("%c ", &escolha);
+            if (escolha == 'r') {
+                printf("Insira o numero de faltas a ser retirado: ");
+                scanf("%d", &mudanca);
+                printf("\n\n");
+                aux->info->quantidade_faltas = aux->info->quantidade_faltas - mudanca;
+                return;
+            }
+            else if (escolha == 'a') {
+                printf("Insira o numero de faltas a ser acrecido: ");
+                scanf("%d", &mudanca);
+                printf("\n\n");
+                aux->info->quantidade_faltas = aux->info->quantidade_faltas + mudanca;
+                return;
+            }
+        }
+        else {
+            printf("Nao foi possivel realizar a mudanca, nome nao encontrado!\n");
+        }
+    }
+}
