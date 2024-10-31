c983d323be52f98f107f0bf73290545f7dacc77a
Nunno Wakiyama Diniz Carvalho <142551010+nunninono@users.noreply.github.com>
2024-10-31 08:26:44 -0300

    Update Playlist Spotify.c

--- Atividades extras/Playlist Spotify.c
+++ Atividades extras/Playlist Spotify.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+typedef struct musica {
+    char[100] titulo_da_musica;
+    char[50] artista;
+    float duracao;
+} Musica;
+
+typedef struct ldecNode {
+    struct ldecNode* ant;
+    Musica* info;
+    struct ldecNode* prox;
+} LDECNode;
+
+typedef struct ldec {
+    LDECNode* inicio;
+    LDECNode* fim;
+    int qtd;
+} LDEC;
+
+void inicializar (LDEC* lista) {
+    lista->inicio = NULL;
+    lista->fim = NULL;
+    lista->qtd = 0;
+}
+
+int isEmpty (LDEC lista) {
+    if (lista.inicio == NULL && lista.fim == NULL) {
+        return 1;
+    }
+    else {
+        return 0;      
+    }
+}
+
+Musica* novaMusica (char* titulo_da_musica, char* artista, float duracao) {}
+    novaMusica = (Musica*) malloc (sizeof(Musica));
+    novaMusica->
+LDECNode*
+
+void adicionarMusica (LDEC* lista)
